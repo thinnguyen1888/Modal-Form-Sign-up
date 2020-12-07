92cbaa7b12ec761ae416beb87c085f91cc314499
thinh nguyen <thinnguyen1888@gmail.com>
2020-12-07 20:52:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import "./App.css";
-import Form from "./components/Form";
+import React, { Component } from "react";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function sendDataCb(data) {
-  console.log(data);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signUp: false,
+      login: true,
+    };
+  }
+
+  updateComponentAppear = (componentName) => {
+    console.log(componentName);
+    if (componentName == "signUp") {
+      this.setState({ signUp: false, login: true });
+    }
+
+    if (componentName == "login") {
+      this.setState({ signUp: true, login: false });
+    }
+  };
+
+  render() {
+    const { signUp, login } = this.state;
+    return (
+      <div>
+        {signUp ? (
+          <SignUp updateComponentAppear={this.updateComponentAppear} />
+        ) : (
+          ""
+        )}
+        {login ? (
+          <Login updateComponentAppear={this.updateComponentAppear} />
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
 }
-
-function App() {
-  return (
-    <div className="App">
-      <Form sendData={sendDataCb} />
-    </div>
-  );
-}
-
 export default App;
